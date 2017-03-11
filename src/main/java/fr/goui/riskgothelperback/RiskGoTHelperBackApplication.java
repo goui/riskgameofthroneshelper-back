7b599c1f8bedc89c4ec79a40b073c1d53ac225ae
goui <guillaume.galliano@gmail.com>
2017-03-11 18:57:43 -0500

    Created intelliJ project

---
+++ src/main/java/fr/goui/riskgothelperback/RiskGoTHelperBackApplication.java
@@ -0,0 +1,12 @@
+package fr.goui.riskgothelperback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskGoTHelperBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskGoTHelperBackApplication.class, args);
+	}
+}
